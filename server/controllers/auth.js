5e0437635212f47d66accac5906f94fd8ae584f5
Viswajith Rajan <viswajithrajan@gmail.com>
2023-09-13 11:17:36 +0530

    first commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,24 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hash });
+
+    await newUser.save();
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT);
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(newUser);
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auths.js";
+
+
+const app = express();
+dotenv.config();
+
+
+const connect = () => {
+    mongoose
+      .connect(process.env.MONGO)
+      .then(() => {
+        console.log("connect to mongodb database");
+      }).catch((err) => {
+        throw err;
+    });
+};
+app.use(express.json)
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+
+app.listen(8000, () => { 
+    connect();
+    console.log("Listening to port 8000");
+});
