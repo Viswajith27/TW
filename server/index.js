4cd1ba15eb046e601c02d4a1929a6d37cfbd6ca1
Viswajith Rajan <viswajithrajan@gmail.com>
2023-09-15 06:54:37 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -6,10 +6,14 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auths.js";
 import tweetRoutes from "./routes/tweets.js";
+const cors = require('cors');
 
 const app = express();
 dotenv.config();
-
+const corsOptions = {
+  origin: 'https://twitter-final-6sqg.onrender.com/api',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
 const connect = () => {
   mongoose.set("strictQuery", false);
   mongoose
@@ -21,7 +25,7 @@ const connect = () => {
       throw err;
     });
 };
-
+app.use(cors(corsOptions))
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/users", userRoutes);
